86f61b4ff26b9ed5ee5aa794b93a8b3df4ceba91
Billiesnyc <billiesnyc@gmail.com>
2018-11-22 17:10:03 +0000

    added seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,27 +7,26 @@ end
 def show
     user = get_current_user
     if user 
-        render json:[user, user.categories]
+        render json:user
     else 
         render json: {error: 'You are not signed in.'}
     end 
 end
 
 def login
-    user = User.find_by(username: params[:username])
+    user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-        render json: {username: user.username, id: user.id, token: issue_token({id: user.id})}
+        render json: {id: user.id, email: user.email, city: user.city, gender: user.gender, token: issue_token({id: user.id})}
     else
-        render json: {error: 'Invalid username/password combination.'}, status: 400
+        render json: {error: 'Invalid email/password combination.'}, status: 400
     end
 end
 
 def signup
-    user = User.new(username: params[:username], password: params[:password])
+    user = User.new(email: params[:email], password: params[:password], city: params[:city], gender: params[:gender] )
     if user.valid?
         user.save
-        CategoryUser.create(category_id: 1, user_id: user.id)
-        render json: {username: user.username, id: user.id, token: issue_token({id: user.id})}
+        render json: {email: user.email, id: user.id, city: user.city, gender: user.gender, token: issue_token({id: user.id})}
     else
         render json: {error: 'Incorrect details'}, status: 400
     end
@@ -36,11 +35,11 @@ end
 def validate
     user = get_current_user
     if user
-        render json: {username: user.username, id: user.id, token: issue_token({id: user.id})}
+        render json: {email: user.email, id: user.id, token: issue_token({id: user.id})}
     else
         render json: {error: 'User not found.'}, status: 400
     end
-    end
+end
 
 
 end
